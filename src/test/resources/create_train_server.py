6567d5c3c473ffdf7d5914798618109675db4a32
Tobias Johansson <tobias@johansson.xyz>
2017-03-27 03:05:19 +0200

    Create tf.train.Server in test

--- src/test/resources/create_train_server.py
+++ src/test/resources/create_train_server.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import sys
 import yarntf
 
-cluster = yarntf.createClusterSpec()
+server = yarntf.createTrainServer()
 
 print('Number of arguments: ' + str(len(sys.argv)))
 print('Argument list: ' + str(sys.argv))
